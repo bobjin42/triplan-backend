7a66367024d82698e2c5a655f1fdbf62fc4f4134
Yicheng Jin <bobjin42@gmail.com>
2018-11-13 21:27:42 -0500

    use regex

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -12,17 +12,17 @@ class Api::V1::TripsController < ApplicationController
 
   def create
     @trip =  Trip.create(trip_params)
-    if Location.all.find {|location| location[:name_suffix].split(",")[0].tr('^A-Za-z', '').downcase.include? (params[:trip][:trip_title].tr('^A-Za-z', '').downcase)}
-      @locations = Location.all.find_all {|location| location[:name_suffix].split(",")[0].tr('^A-Za-z', '').downcase.include? (params[:trip][:trip_title].tr('^A-Za-z', '').downcase)}
+    if Location.all.find {|location| location[:name_suffix].split(",")[0].gsub(/[^a-zA-Z]/, '').downcase.include? (params[:trip][:trip_title].gsub(/[^a-zA-Z]/, '').downcase)}
+      @locations = Location.all.find_all {|location| location[:name_suffix].split(",")[0].gsub(/[^a-zA-Z]/, '').downcase.include? (params[:trip][:trip_title].gsub(/[^a-zA-Z]/, '').downcase)}
       render json: @locations
     else
-      cityCode = (Code.all.find { |city| city["city_name"].tr('^A-Za-z', '').downcase == params[:trip][:trip_title].tr('^A-Za-z', '').downcase} || Code.all.find { |city| city["city_name"].tr('^A-Za-z', '').downcase.include? params[:trip][:trip_title].tr('^A-Za-z', '').downcase}).city_code
+      cityCode = (Code.all.find { |city| city["city_name"].gsub(/[^a-zA-Z]/, '').downcase == params[:trip][:trip_title].gsub(/[^a-zA-Z]/, '').downcase} || Code.all.find { |city| city["city_name"].gsub(/[^a-zA-Z]/, '').downcase.include? params[:trip][:trip_title].gsub(/[^a-zA-Z]/, '').downcase}).city_code
       url = "https://api.sygictravelapi.com/1.1/en/places/list?parents=#{cityCode}&level=poi&limit=100"
       response = HTTParty.get(url, :headers => { "Content-Type" => "application/json", "x-api-key" => Rails.application.credentials.api})
       response["data"]["places"].each do |location|
         Location.create(api_id: location["id"], lat: location["location"]["lat"], lng: location["location"]["lng"], name: location["name"], name_suffix: location["name_suffix"], url: location["url"], categories: location["categories"], perex: location["perex"], thumbnail_url: location["thumbnail_url"], rating: location["rating"])
       end
-      @locations = Location.all.find_all {|location| location[:name_suffix].split(",")[0].tr('^A-Za-z', '').downcase.include? (params[:trip][:trip_title]..tr('^A-Za-z', '').downcase)}
+      @locations = Location.all.find_all {|location| location[:name_suffix].split(",")[0].gsub(/[^a-zA-Z]/, '').downcase.include? (params[:trip][:trip_title]..gsub(/[^a-zA-Z]/, '').downcase)}
       render json: @locations
     end
   end
